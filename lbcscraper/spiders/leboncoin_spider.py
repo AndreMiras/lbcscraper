cc4cc33857e3f1e0cedeffa058d8b88a007c6b92
Andre Miras <andre.miras@gmail.com>
2015-03-19 21:11:23 +0100

    Apartment draft scraper

---
+++ lbcscraper/spiders/leboncoin_spider.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from lbcscraper.items import LbcItem
+
+
+class LeboncoinSpider(scrapy.Spider):
+    name = "leboncoin"
+    allowed_domains = ["leboncoin.fr"]
+    start_urls = (
+        # 'http://www.leboncoin.fr/',
+        # 'http://www.leboncoin.fr/ventes_immobilieres/offres/languedoc_roussillon/herault/',
+    )
+
+    def __init__(self, *args, **kwargs): 
+        """
+        Retrieves start_url from command line options:
+        scrapy crawl leboncoin -a start_url="http://www.leboncoin.fr/ventes_immobilieres/offres/languedoc_roussillon/herault/"
+        """
+        super(LeboncoinSpider, self).__init__(*args, **kwargs) 
+        self.start_urls = [kwargs.get('start_url')] 
+
+    def custom_start_requests(self):
+        for url in self.start_urls:
+            yield self.make_requests_from_url(url)
+
+    def parse(self, response):
+        ads_elems = response.xpath('//div[@class="list-lbc"]/a')
+        for ad_elem in ads_elems:
+            item = LbcItem()
+            link = ad_elem.xpath('@href').extract()
+            item['link'] = link
+            details_elem = ad_elem.xpath('div[@class="lbc"]/div[@class="detail"]')
+            title = details_elem.xpath('div[@class="title"]/text()').extract()
+            item['title'] = [s.strip() for s in title]
+            price = details_elem.xpath('div[@class="price"]/text()').extract()
+            item['price'] = [s.replace(u"\xa0€", "").replace(" ", "").strip() for s in price]
+            request = scrapy.Request(link[0], callback=self.parse_details)
+            request.meta['item'] = item
+            # yield item
+            yield request
+
+    def parse_details(self, response):
+        item = response.meta['item']
+        surface_area = response.xpath('//div[contains(@class, "criterias")]/table/tr/th[contains(text(), "Surface :")]/following-sibling::td/text()').extract()
+        item['surface_area'] = [s.replace(" m", "") for s in surface_area]
+        city = response.xpath('//table/tr/th[contains(text(), "Ville :")]/following-sibling::td/text()').extract()
+        item['city'] = city
+        postcode = response.xpath('//table/tr/th[contains(text(), "Code postal :")]/following-sibling::td/text()').extract()
+        item['postcode'] = postcode
+        # yield item
+        return item
